06d4f142a1e563a451d9494572255775d3d22704
Mitchell Dodell <mdodell@hubspot.com>
2024-07-12 15:03:14 -0400

    Do simple handshake setup without storing tokens

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.development',
+    }),
+    HttpModule.register({
+      baseURL: 'https://api.hubapi.com/oauth/v1/token',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/dtos/oauth/access-token.dto.ts
@@ -0,0 +1,4 @@
+export class HandshakeDTO {
+  readonly access_token: string;
+  readonly refresh_token: string;
+}

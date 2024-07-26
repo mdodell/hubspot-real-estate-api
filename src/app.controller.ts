5a29aab6da8a36d1e06fe239ba5649c3d1082af3
kimunz <kimunz@hubspot.com>
2024-07-26 11:05:43 -0400

    Remove curl with expired access token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -21,17 +21,3 @@ export class AppController {
     });
   }
 }
-
-/*
-curl --request POST \
-  --url https://api.hubapi.com/crm/v3/objects/products \
-  --header 'authorization: Bearer COHFn-CMMhIEAAECQBjhu5sWIKmOhwwogPDZATIUilW2YaFw9ntYdU-YfTzEk4WZyvU6MAAAAEEAAAAAAPgDAAAAAAAAgAAAAAAAAAAAAAAAAAAAAGAAAAAAAAAAAAAAAAAAAkIUJs48yZhHzgxac2PQGld7zk0kAmpKA25hMVIAWgBgAA' \
-  --header 'content-type: application/json' \
-  --data '{
-  "properties": {
-    "zip": "10451",
-    "name": "silly house",
-    "price": "10000000"
-  }
-}'
-*/

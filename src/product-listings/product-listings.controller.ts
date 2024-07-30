0b1ca4702c60cb59ac0ae6afff0e9e418176a86b
Mitchell Dodell <mdodell@hubspot.com>
2024-07-30 17:24:16 -0400

    Log things

--- src/product-listings/product-listings.controller.ts
+++ src/product-listings/product-listings.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get, Inject, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Inject,
+  Logger,
+  Query,
+  Request,
+} from '@nestjs/common';
 import { ProductListingsService } from './product-listings.service';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 
 @Controller('product-listings')
 export class ProductListingsController {
+  private readonly logger = new Logger(ProductListingsController.name);
   constructor(
     private readonly productListingService: ProductListingsService,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
@@ -25,12 +33,17 @@ export class ProductListingsController {
 
   @Get('/get-listings-for-zip')
   async getListingsForZip(
+    @Request() request: Request,
     @Query('zip') zip: string,
     @Query('portalId') portalId,
   ) {
+    this.logger.log('REQUEST');
+    this.logger.log(request.toString());
     const accessToken = await this.cacheManager.get<string>(
       `${portalId}-accessToken`,
     );
+
+    this.logger.log(`Access Token: ${accessToken}`);
     const result = await this.productListingService.getListingsForZip(
       accessToken,
       zip,
